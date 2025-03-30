bc8d1ab2a2f86e8ac828dff807d2082ee555cfb0
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-30 12:05:46 +0200

    Add header to blog post display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,27 @@ export default function App() {
 
   return (
 
-    <ul>
-      {blogs.map((blog) => (
-        <div className="container" key={blog.slug}>
-          <li>
-            <img src={blog.image} alt={blog.slug} />
-            <h3>{blog.title}</h3>
-            <p>{blog.content}</p>
-          </li>
-        </div>
-      ))}
-    </ul>
+    <>
+
+      <div className="header text-center my-4">
+        <h1>Posts</h1>
+      </div>
+
+      <ul>
+        {blogs.map((blog) => (
+          <div className="container" key={blog.slug}>
+            <li style={{ listStyle: "none" }}>
+              <img src={blog.image} alt={blog.slug} />
+              <h3>{blog.title}</h3>
+              <p>{blog.content}</p>
+            </li>
+          </div>
+        ))}
+      </ul>
+
+    </>
+
+
   )
 
 }
